79210a6838d1bfdee4eb9933050fec423e1b9124
Job Karani <moringa@moringa-Thinkpad-L450>
2021-12-30 02:33:46 +0300

    initial commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+# Create your views here.
+
+
+def index(request):
+
+    return render(request, 'all-temps/index.html')
