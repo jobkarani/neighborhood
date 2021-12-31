f4a3a9bde716efce95b59ee348d718e80127dce6
Job Karani <moringa@moringa-Thinkpad-L450>
2021-12-31 13:56:10 +0300

    creates contact model

--- app/models.py
+++ app/models.py
@@ -22,7 +22,7 @@ class Neighbourhood(models.Model):
     name = models.CharField(max_length=20)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     occupants_count = models.IntegerField(default=0)
-    admin = models.ForeignKey(Uswe, on_delete=models.CASCADE)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -44,17 +44,18 @@ class Neighbourhood(models.Model):
         hood = cls.objects.get(id=id)
         return hood
 
-    @classmethod()
+    @classmethod
     def update_hood(cls, id):
         cls.objects.filter(id=id).update()
 
-    @classmethod()
+    @classmethod
     def update_occupants(cls, occupants_count):
         cls.objects.filter(occupants_count=occupants_count).update()
 
 
-class User(model.Models):
+class User(models.Model):
     name = models.CharField(max_length=20)
+    profile_pic = CloudinaryField('image')
     email = models.EmailField(max_length=30)
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
@@ -68,7 +69,7 @@ class User(model.Models):
 
 class Business(models.Model):
     name = models.CharField(max_length=20)
-    email = model.EmailField(max_length=30)
+    email = models.EmailField(max_length=30)
     about = models.TextField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
@@ -92,6 +93,36 @@ class Business(models.Model):
     def find_business(cls, id):
         business = cls.objects.get(id=id)
         return business
+
+    def __str__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    name = models. CharField(max_length=20)
+    email = models.EmailField(max_length=30, blank=True, null=True)
+    phone_number = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def create_contact(self):
+        self.save()
+
+    def update_contact(self):
+        self.update()
+
+    def delete_contact(self):
+        self.delete()
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        contact = cls.objects.filter(name__icontains=search_term)
+        return contact
+
+    @classmethod
+    def find_contact(cls, id):
+        contact = cls.objects.get(id=id)
+        return contact
 
     def __str__(self):
         return self.name
