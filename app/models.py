fc55aa3972b869345a51165f104ba8fdf49dd15c
Job Karani <moringa@moringa-Thinkpad-L450>
2021-12-31 12:18:16 +0300

    creates location model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+import datetime as dt
+from tinymce.models import HTMLField
 
 # Create your models here.
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=20)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def save_loc(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
+from .models import *
+
 # Create your views here.
 
 
