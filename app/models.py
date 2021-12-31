df27f2e9d505a312a9286ac176d3c137b5d22c39
Job Karani <moringa@moringa-Thinkpad-L450>
2021-12-31 12:48:26 +0300

    creates class neighbourhood

--- app/models.py
+++ app/models.py
@@ -16,3 +16,38 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=20)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    occupants_count = models.IntegerField(default=0)
+    admin = models.ForeignKey(Uswe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def create_hood(self):
+        self.save()
+
+    @classmethod
+    def delete_hood(cls, id):
+        cls.objects.filter(id=id).delete()
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        hood = cls.objects.filter(name__icontains=search_term)
+        return hood
+
+    @classmethod
+    def find_hood(cls, id):
+        hood = cls.objects.get(id=id)
+        return hood
+
+    @classmethod()
+    def update_hood(cls, id):
+        cls.objects.filter(id=id).update()
+
+    @classmethod()
+    def update_occupants(cls, occupants_count):
+        cls.objects.filter(occupants_count=occupants_count).update()
