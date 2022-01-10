bed628f0fd8e95a9c1c51752857a1b0479733e53
Job Karani <moringa@moringa-Thinkpad-L450>
2022-01-10 07:38:04 +0300

    creates neighbourhood form

--- app/forms.py
+++ app/forms.py
@@ -14,8 +14,16 @@ class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['photo', 'bio', 'contact', 'location', 'hood']
-        
+
+
 class BusinessForm(forms.ModelForm):
     class Meta:
-        model=Business
-        fields=['name','email','about','location','hood']
+        model = Business
+        fields = ['name', 'email', 'about', 'location', 'hood']
+
+
+class NeighbourhoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighbourhood
+        fields = ['photo', 'name', 'about',
+                  'location', 'cop_dail', 'health_dail']
