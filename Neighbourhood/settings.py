16704a10db9e981484e53391a532700b56f04bfc
Job Karani <moringa@moringa-Thinkpad-L450>
2022-01-11 20:37:05 +0300

    styles navbar

--- Neighbourhood/settings.py
+++ Neighbourhood/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-3gpf5@m^juwv*iz=1$zp2yf9sl-+9oxwiovvnt_w7oqagk3)86'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -100,16 +100,25 @@ WSGI_APPLICATION = 'Neighbourhood.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': '',
+#     }
+
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neighbourhood',
+        'USER': 'moringa',
+        'PASSWORD': 'Access',
     }
-
 }
 
 # Password validation
